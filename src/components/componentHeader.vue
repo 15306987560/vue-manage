e60b43eca57cbd83c64a652e6c1d7da1cd4e8684
LJM <3106577933@qq.com>
2022-10-18 17:09:20 +0800

    测试2

--- src/components/componentHeader.vue
+++ src/components/componentHeader.vue
@@ -4,7 +4,7 @@
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item>首页</el-breadcrumb-item>
       <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动列</el-breadcrumb-item>
       <el-breadcrumb-item>活动详</el-breadcrumb-item>
     </el-breadcrumb>
   </div>
